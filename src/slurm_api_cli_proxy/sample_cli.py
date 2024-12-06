68d9453719b95d84b28e87daa8f3fb03e2cd7afc
Hector Cadavid <hector.cadavid@gmail.com>
2024-12-06 17:35:44 +0100

    sample client working with the stub API

--- src/slurm_api_cli_proxy/sample_cli.py
+++ src/slurm_api_cli_proxy/sample_cli.py
@@ -1,12 +1,33 @@
 import openapi_client
+import os
 
 from openapi_client.rest import ApiException
 from pprint import pprint
 
 configuration = openapi_client.Configuration(
 
-    host = "https://virtserver.swaggerhub.com/hcadavid6/slurm-rest_api_ro/0.0.38"
+    host = "http://localhost:8080/hcadavid6/slurm-rest_api_ro/0.0.38"
+
+    #host = "https://virtserver.swaggerhub.com/hcadavid6/slurm-rest_api_ro/0.0.38"
 )
+
+# The client must configure the authentication and authorization parameters
+# in accordance with the API server security policy.
+# Examples for each auth method are provided below, use the example that
+# satisfies your auth use case.
+
+# Configure API key authorization: user
+configuration.api_key['user'] = os.environ["API_KEY"]
+
+# Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
+# configuration.api_key_prefix['user'] = 'Bearer'
+
+# Configure API key authorization: token
+configuration.api_key['token'] = os.environ["API_KEY"]
+
+# Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
+# configuration.api_key_prefix['token'] = 'Bearer'
+
 
 # Enter a context with an instance of the API client
 with openapi_client.ApiClient(configuration) as api_client:
