3a01c4d3f9133aebef5f9849f7a3479d46abe87a
Carsten Schelp <carsten.schelp@surf.nl>
2026-03-19 15:05:48 +0100

    add metadata in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 import os
+from pathlib import Path
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
 slurm_api_client_path = os.path.join(current_dir, 'slurm_api_client')
+long_description = (current_dir / "README.md").read_text()
 
 
 # Generate requirements from requirements.txt
@@ -10,6 +12,8 @@ requirements = open(os.path.join(os.path.dirname(__file__), 'requirements.txt'))
 
 setup(
     name="slurm_api_cli_proxy",
+    description="Translate SLURM CLI commands to SLURM REST API calls",
+    author="Hector Cadavid, Carsten Schelp",
     version="0.2.1",
     packages= ["src", "slurm_api_client"],
     include_package_data=True,
@@ -23,4 +27,15 @@ setup(
             "scontrol=slurm_api_cli_proxy.command_handler:scontrol",
         ],
     },
+    licence="Apache-2.0",
+    keywords='slurm cli rest api proxy',
+    url="https://github.com/SLURM-CLI-API-Proxy/SLURM-CLI-API-Proxy-client/tree/main",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+      "License :: OSI Approved :: Apache Software License 2.0",
+      "Development Status :: 3 - Alpha",
+      "Intended Audience :: Developers",
+      "Programming Language :: Python :: 3.12",
+    ],
 )
