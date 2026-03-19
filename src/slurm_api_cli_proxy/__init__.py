4aad6c7880155ff4b0be8571aafb7a28c839047f
Carsten Schelp <carsten.schelp@surf.nl>
2026-03-19 16:10:59 +0100

    bump all version mentions

--- src/slurm_api_cli_proxy/__init__.py
+++ src/slurm_api_cli_proxy/__init__.py
@@ -6,4 +6,4 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 
 __author__ = "Hector Cadavid"
 __email__ = "h.cadavid@esciencecenter.nl"
-__version__ = "0.2.1"
+__version__ = "0.2.2"
